13f160ba8f7eea942db9e8503d1d73cd5f998951
jesskaiser <124453049+jesskaiser@users.noreply.github.com>
2023-03-05 00:40:44 +0100

    updated scripts.js
    created a blank array and added six objects to it

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
-alert('Hello world');
-let favoriteFood = 'Spaghetti Bolognese';
-document.write(favoriteFood);
+// create pokemonList variable and assign it to a blank array
+let pokemonList = [];
+
+//add pokemon objects to pokemonList array
+pokemonList[0] = { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] };
+pokemonList[1] = { name: 'Ivysaur', height: 1.0, type: ['grass', 'poison'] };
+pokemonList[2] = { name: 'Venusaur', height: 2.0, type: ['grass', 'poison'] };
+pokemonList[3] = { name: 'Charmander', height: 0.6, type: 'fire' };
+pokemonList[4] = { name: 'Charmeleon', height: 1.1, type: 'fire' };
+pokemonList[5] = { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] };
