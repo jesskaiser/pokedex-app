d9036c2c4ca543872456d57596e7fd338e5c6282
jesskaiser <124453049+jesskaiser@users.noreply.github.com>
2023-03-05 23:55:09 +0100

    updated scripts.js
    Created a loop that iterates over the array and writes the name and height of every element on the website's DOM, and only labels the biggest item on the list

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,21 @@ let pokemonList = [
   { name: 'Ivysaur', height: 1.0, type: ['grass', 'poison'] },
   { name: 'Venusaur', height: 2.0, type: ['grass', 'poison'] },
   { name: 'Charmander', height: 0.6, type: 'fire' },
-  { name: 'Charmeleon', height: 1.1, type: 'fire' },
-  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] }
+ { name: 'Charmeleon', height: 1.1, type: 'fire' },
+  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] },
 ];
+
+//create a for loop to iterate over the pokemonList and write to DOM, creating separate <div> for each element for styling
+let i = 0;
+let text = '';
+for (; pokemonList[i];) {
+  text = pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ';
+  if (pokemonList[i].height > 1.7) {
+    //add label to the biggest Pokemon 
+    text += '<p>- Wow, that\'s big!</p>';
+  }
+    //create separate <div> for each element -> for CSS styling
+  document.write(`<div> ${text} </div>`);
+  i++;
+}
+
