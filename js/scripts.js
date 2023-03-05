52b34b2138fda6910d4555bccf690f13b928d9d6
jesskaiser <124453049+jesskaiser@users.noreply.github.com>
2023-03-05 08:05:21 +0100

    updated scripts.js
    variable declaration and initialisation in a single line

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-// create pokemonList variable and assign it to a blank array
-let pokemonList = [];
-
-//add pokemon objects to pokemonList array
-pokemonList[0] = { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] };
-pokemonList[1] = { name: 'Ivysaur', height: 1.0, type: ['grass', 'poison'] };
-pokemonList[2] = { name: 'Venusaur', height: 2.0, type: ['grass', 'poison'] };
-pokemonList[3] = { name: 'Charmander', height: 0.6, type: 'fire' };
-pokemonList[4] = { name: 'Charmeleon', height: 1.1, type: 'fire' };
-pokemonList[5] = { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] };
+// create pokemonList array and added Pokemon objects to it
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison'] },
+  { name: 'Ivysaur', height: 1.0, type: ['grass', 'poison'] },
+  { name: 'Venusaur', height: 2.0, type: ['grass', 'poison'] },
+  { name: 'Charmander', height: 0.6, type: 'fire' },
+  { name: 'Charmeleon', height: 1.1, type: 'fire' },
+  { name: 'Charizard', height: 1.7, type: ['fire', 'flying'] }
+];
