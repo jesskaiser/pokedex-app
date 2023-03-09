b513d0438b380fa176df0822f2e6f796c45f47bc
jesskaiser <124453049+jesskaiser@users.noreply.github.com>
2023-03-09 09:11:06 +0100

    updated scripts.js with bonus tasks
    -add pokemon function: check if the pokemon is a type of object and
    
    -has the same number of keys  as the first pokemon added, with each key matching those keys
    
    -find a pokemon by its name in the list and change its corresponding div background colour
    and set div id to each pokemon's name for individual styling

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,17 @@ let pokemonRepository = (function () {
   return {
     //add a single item to the pokemonList array 
     add: function (pokemon) {
-      pokemonList.push(pokemon);
+      //check if the pokemon is an object and has the same number of keys as the first item in the list ( === 3)
+      if (typeof pokemon === 'object' && Object.keys(pokemon).length === Object.keys(pokemonList[0]).length) {
+        //check if each key is the same as in the pokemonList (name, height and type)
+        if (Object.prototype.hasOwnProperty.call(pokemon, 'name')) {
+          if (Object.prototype.hasOwnProperty.call(pokemon, 'height')) {
+            if (Object.prototype.hasOwnProperty.call(pokemon, 'type')) {
+              pokemonList.push(pokemon);
+            }
+          }
+        }
+      }
     },
     //return all items from pokemonList
     getAll: function () {
@@ -20,6 +30,9 @@ let pokemonRepository = (function () {
     }
   }
 })();
+//test the add function
+pokemonRepository.add({ name: 'Pikachu', height: 1.2, type: 'water' });
+
 //create a for loop to iterate over the pokemonList and write to DOM, creating separate <div> for each element for styling
 pokemonRepository.getAll().forEach(function(pokemon) {
   let text = pokemon.name + ' (height: ' + pokemon.height + ') ';
@@ -27,7 +40,18 @@ pokemonRepository.getAll().forEach(function(pokemon) {
     //add label to the biggest Pokemon 
     text += '<p>- Wow, that\'s big!</p>';
   }
-   //create separate <div> for each element -> for CSS styling
-  document.write(`<div> ${text} </div>`);
+   //create separate <div> for each element with div id set to name -> for CSS styling
+  document.write(`<div id="${pokemon.name}"> ${text} </div>`);
 });
-
+//find a pokemon in the list, if it exists find its div id and change its background color 
+let findPokemon = function (name) {
+  pokemonRepository.getAll().filter(function (pokemon) {
+    if (pokemon.name === name) {
+      document.getElementById(`${pokemon.name}`).style.backgroundColor = '#acfaae';
+      console.log(pokemon.name + ' is found');
+    } 
+  })
+};
+//test the findPokemon function
+findPokemon('Ivysaur');
+findPokemon('ladida');
