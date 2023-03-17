8c701b192f90d94df65437a420e72ea359a7709e
jesskaiser <124453049+jesskaiser@users.noreply.github.com>
2023-03-17 18:04:49 +0100

    replaced the static list with a complete list loaded from an API
    -replaced the array list with an empty list
    -added loadList() and loadDetails() function inside the data repository to load data from an external source
    -add() function keys changed to name and detailsUrl
    -showDetails() function now loads the Pokémon details from the API instead of loading static data
    -added showLoadingMessage() and hideLoadingMessage() functions to display and hide a loading message while data is being loaded

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,14 @@
 //pokemonRepository variable to hold what the  IIFE will return, then assign the IIFE to that variable
 let pokemonRepository = (function() {
-  // create pokemonList array and added Pokemon objects to it
-  let pokemonList = [{
-      name: 'Bulbasaur',
-      height: 0.7,
-      type: ['grass', 'poison']
-    },
-    {
-      name: 'Ivysaur',
-      height: 1.0,
-      type: ['grass', 'poison']
-    },
-    {
-      name: 'Venusaur',
-      height: 2.0,
-      type: ['grass', 'poison']
-    },
-    {
-      name: 'Charmander',
-      height: 0.6,
-      type: 'fire'
-    },
-    {
-      name: 'Charmeleon',
-      height: 1.1,
-      type: 'fire'
-    },
-    {
-      name: 'Charizard',
-      height: 1.7,
-      type: ['fire', 'flying']
-    },
-  ];
+  // create an empty pokemonList array
+  let pokemonList = [];
+  //API to load from
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   //add a single item to the pokemonList array
   function add(pokemon) {
-    //check if the pokemon is an object and has the same number of keys as the first item in the list ( === 3)
-    if (typeof pokemon === 'object' && Object.keys(pokemon).length === Object.keys(pokemonList[0]).length) {
-      //check if each key is the same as in the pokemonList (name, height and type)
-      if (Object.prototype.hasOwnProperty.call(pokemon, 'name')) {
-        if (Object.prototype.hasOwnProperty.call(pokemon, 'height')) {
-          if (Object.prototype.hasOwnProperty.call(pokemon, 'type')) {
-            pokemonList.push(pokemon);
-          }
-        }
-      }
+    //check if the pokemon is an object and has name and detailsUrl keys
+    if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon) {
+      pokemonList.push(pokemon);
     }
   }
 
@@ -57,7 +22,8 @@ let pokemonRepository = (function() {
     let listItem = document.createElement('li');
     let button = document.createElement('button');
     button.type = 'button';
-    button.innerText = pokemon.name;
+    //Pokemon names to start with uppercase letter
+    button.innerText = pokemon.name.charAt(0).toUpperCase()+pokemon.name.slice(1);
     button.classList.add('button');
     //add pokemon name as ID to li item
     listItem.setAttribute('id', `${pokemon.name}`);
@@ -76,55 +42,114 @@ let pokemonRepository = (function() {
 
   //show pokemon details
   function showDetails(pokemon) {
-    console.log(pokemon.name);
-    //test
-    //document.getElementById(`${pokemon.name}`).firstElementChild.style.background = '#acdade';
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon.name);
+    });
   }
 
   //return all items from pokemonList
   function getAll() {
     return pokemonList;
   }
-  
+  //load the list of Pokemon, fetch data from API
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function (response) {
+      //method: 'GET'
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        hideLoadingMessage();
+      });
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    })
+  }
+  //load detailed data of a pokemon
+  function loadDetails(item) {
+    showLoadingMessage();
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      //add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+  }
+  //show a loading message 
+  function showLoadingMessage() {
+    let loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.classList.remove('hidden');
+  }
+  //hide a loading message
+  function hideLoadingMessage() {
+    let loadingMessage = document.querySelector('.loading-message');
+    loadingMessage.classList.add('hidden');
+  }
+
   return {
     add: add,
     addListItem: addListItem,
     displayPokemon: displayPokemon,
     showDetails: showDetails,
-    getAll: getAll
+    getAll: getAll,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showLoadingMessage: showLoadingMessage,
+    hideLoadingMessage: hideLoadingMessage
   }
 })();
 
-//test the add function
-pokemonRepository.add({
-  name: 'Pikachu',
-  height: 0.3,
-  type: 'electric'
-});
-
-//create a for loop to iterate over the pokemonList and write to DOM, creating separate <div> for each element for styling
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-  //create separate <div> for each element with div id set to name -> for CSS styling
-  // document.write(`<div id="${pokemon.name}"> ${text} </div>`);
+pokemonRepository.loadList().then(function () {
+  // Now the data is loaded!
+  //create a for loop to iterate over the pokemonList and write to DOM
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+    //create separate <div> for each element with div id set to name -> for CSS styling
+    // document.write(`<div id="${pokemon.name}"> ${text} </div>`);
+  });
 });
 
 //find a pokemon in the list, if it exists find its div id and change its background color 
-let findPokemon = function(name) {
-  //create an arraylist of pokemon names for filter function
+let findPokemon = function (name) {
+  //set name to start with lower case
+  name = name.charAt(0).toLowerCase() + name.slice(1);
+   //create an arraylist of pokemon names for filter function
   let pokemonNames = [];
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonNames.push(pokemon.name);
-  })
-  //filter function to find a pokemon's name on the pokemonNames list
-  pokemonNames.filter(function(pokemon) {
-    if (pokemon === name) {
-      document.getElementById(`${pokemon}`).firstElementChild.style.background = '#acdade';
-      console.log(pokemon + ' is found');
-    }
-  })
+  pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+      pokemonNames.push(pokemon.name);
+    })
+    console.log(pokemonNames);
+    //filter function to find a pokemon's name on the pokemonNames list
+    pokemonNames.filter(function (pokemon) {
+      if (pokemon === name) {
+        document.getElementById(`${pokemon}`).firstElementChild.style.background = '#acdade';
+        console.log(name + ' is found');
+      }
+    })
+})
 };
+
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded
+  pokemonRepository.getAll().forEach(function(pokemon){
+    pokemonRepository.addListItem(pokemon);
+  });
+});
 //test the findPokemon function
 //findPokemon('Ivysaur');
-//findPokemon('Bulbasaur');
+//findPokemon('bulbasaur');
 //findPokemon('ladida');
